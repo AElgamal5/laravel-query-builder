b3ebcdc6ec29fef5284616db533cf0c923ed4424
elgamal <ahmedelgamal5320@gmail.com>
2024-11-25 20:27:57 +0200

    chore: back to base

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Role;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Spatie\QueryBuilder\QueryBuilder;
@@ -11,22 +10,11 @@ class UserController extends Controller
 {
     public function index(Request $request)
     {
-        $query = $this->queryBuilder();
 
-        $users = $query->get();
+        $users = QueryBuilder::for(User::class)
+            ->allowedFilters([])
+            ->get();
 
         return response()->json($users);
-    }
-
-    private function queryBuilder(): QueryBuilder
-    {
-        $cols = User::columnList();
-
-        return QueryBuilder::for(User::class)
-            ->allowedFilters($cols)
-            ->defaultSort("-created_at")
-            ->allowedSorts($cols)
-            ->allowedFields([...$cols, ...Role::columnList('role')])
-            ->allowedIncludes(['role']);
     }
 }
--- app/Models/Role.php
+++ app/Models/Role.php
@@ -2,14 +2,13 @@
 
 namespace App\Models;
 
-use App\Traits\HasColumnList;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
 
-    use HasFactory, HasColumnList;
+    use HasFactory;
 
     protected $fillable = [
         'name',
