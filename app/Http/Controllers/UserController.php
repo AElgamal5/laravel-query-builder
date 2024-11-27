906f97e84c9ea3381604d7b3ff2b15c6c7899125
elgamal <ahmedelgamal5320@gmail.com>
2024-11-27 13:33:18 +0200

    feat: finish sorting

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Models\Role;
 use App\Models\User;
+use App\Sorts\StringLengthSort;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
+use Spatie\QueryBuilder\AllowedSort;
 use Spatie\QueryBuilder\QueryBuilder;
 
 class UserController extends Controller
@@ -13,8 +16,26 @@ class UserController extends Controller
     {
         $users = QueryBuilder::for(User::class)
             ->allowedSorts([
-                
+                //basic
+                'id',
+                'name',
+
+                //custom
+                // AllowedSort::custom('name-length', new StringLengthSort(), 'name'),
+
+                //alias
+                // AllowedSort::field('num', 'id'),
+
+                //callback
+                // AllowedSort::callback('role-name', function (Builder $query, $direction) {
+                //     $query->whereHas('role', function ($roleQuery) use ($direction) {
+                //         $roleQuery->orderBy('name', $direction ? 'asc' : 'desc');
+                //     });
+                // })
             ])
+            // ->defaultSort('-created_at')
+            // ->defaultSort('-created_at', 'name')
+            // ->with('role')
             ->get();
 
         return response()->json($users);
