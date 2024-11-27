30008c00c33e0a1fd0f53f908f262e66168d5185
elgamal <ahmedelgamal5320@gmail.com>
2024-11-27 14:37:50 +0200

    feat: finish includes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,28 +5,36 @@ namespace App\Http\Controllers;
 use App\Models\Role;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Spatie\QueryBuilder\AllowedInclude;
 use Spatie\QueryBuilder\QueryBuilder;
 
 class UserController extends Controller
 {
     public function index(Request $request)
     {
-        $query = $this->queryBuilder();
+        $users = QueryBuilder::for(User::class)
+            ->allowedIncludes([
+                //basic
+                // 'role',
 
-        $users = $query->get();
+                //nested
+                // 'role.permissions',
+
+                //count
+                // AllowedInclude::count('roleCount'),
+
+                //exist
+                // AllowedInclude::exists('roleExists'),
+
+                //note
+                // 'role',  // allows including `role` or `roleCount` or `roleExists`
+
+                //aliases
+                // AllowedInclude::relationship('rank', 'role'),
+                // AllowedInclude::relationship('rank.authorities', 'role.permissions')
+            ])
+            ->get();
 
         return response()->json($users);
-    }
-
-    private function queryBuilder(): QueryBuilder
-    {
-        $cols = User::columnList();
-
-        return QueryBuilder::for(User::class)
-            ->allowedFilters($cols)
-            ->defaultSort("-created_at")
-            ->allowedSorts($cols)
-            ->allowedFields([...$cols, ...Role::columnList('role')])
-            ->allowedIncludes(['role']);
     }
 }
